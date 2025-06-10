75b78218ebf0580d576bfcfc66de254be0315462
Rahul-college-use <rahulcollegeuse@gmail.com>
2025-06-10 11:32:45 +0530

    toggle image in header file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './Header.css';
 
 const Header = ({ darkMode, toggleDarkMode }) => {
-  const profileImageUrl = "https://avatars.githubusercontent.com/u/181213962?v=4";
 
+  const profileImageUrl = darkMode ? `https://i.ibb.co/Gv1T33nt/Whats-App-Image-2025-06-05-at-21-21-03-e0ff578d.jpg` : `https://avatars.githubusercontent.com/u/181213962?v=4`;
   return (
     <header className="portfolio-header" style={darkMode ? { background: "#1F2932" } : {}}>
       <div className="header-content">
@@ -11,7 +11,9 @@ const Header = ({ darkMode, toggleDarkMode }) => {
           <img
             src={profileImageUrl}
             alt="Profile"
-            className="profile-image"
+            className={`profile-image ${darkMode ? "dark" : "light"} fade-in`}
+            title='Hi Everyone , Follow on Instagram'
+            onClick={()=>window.open('https:/instagram.com/_rahul_x_4u_')}
           />
         </div>
         <h1>Rahul Kumar</h1>
