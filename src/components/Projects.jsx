6c41acb23793a64bdf743369b5ba431e54212d44
RAHUL KUMAR <rahulcollegeuse@gmail.com>
2025-06-14 10:09:24 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,14 +6,14 @@ const Projects = ({darkMode}) => {
     // Initialize with empty array if undefined
     const [projects] = useState([
         {
-            id: 1,
-            title: "Resturarent Platform",
-            description: "A full-stack Resturarents application with payment integration",
-            technologies: ["React", "Node.js"],
+            id: 3,
+            title: "Weather Application Platform",
+            description: "Weather  application real time Report  integration",
+            technologies: ["React", "Node.js","newsApi"],
             //   image: "https://images.unsplash.com/photo-1555529669-e69e7aa0ba9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=80",
-            image: 'https://i.ibb.co/vCTZfsPR/Screenshot-2025-06-08-154500.png',
-            details: "Developed a complete Resturarents solution with user authentication...",
-            link: "https://restuarents-app-h-git-main-rahul-college-uses-projects.vercel.app/"
+            image: 'https://i.ibb.co/DHKMM9Ht/brave-screenshot-localhost.png',
+            details: "Developed a complete Weather App...",
+            link: "https://weather-app-8xf6.vercel.app/"
         },
         {
             id: 2,
@@ -25,16 +25,17 @@ const Projects = ({darkMode}) => {
             details: "Developed a complete News solution...",
             link: "https://prime-source-ttt.vercel.app/"
         },
+
         {
-            id: 3,
-            title: "Weather Application Platform",
-            description: "Weather  application real time Report  integration",
-            technologies: ["React", "Node.js","newsApi"],
+            id: 1,
+            title: "Resturarent Platform",
+            description: "A full-stack Resturarents application with payment integration",
+            technologies: ["React", "Node.js"],
             //   image: "https://images.unsplash.com/photo-1555529669-e69e7aa0ba9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=80",
-            image: 'https://i.ibb.co/DHKMM9Ht/brave-screenshot-localhost.png',
-            details: "Developed a complete Weather App...",
-            link: "https://prime-source-ttt.vercel.app/"
-        },
+            image: 'https://i.ibb.co/vCTZfsPR/Screenshot-2025-06-08-154500.png',
+            details: "Developed a complete Resturarents solution with user authentication...",
+            link: "https://restuarents-app-h-git-main-rahul-college-uses-projects.vercel.app/"
+        }
 
         // Add more projects as needed
     ]);
